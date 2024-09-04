b36f3f6f5e45dd199fa0be7089c081b7c29367e8
Deepak  Joshi <dj753233@gmail.com>
2024-09-04 12:05:27 +0700

    Update HomePage.jsx

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,28 +1,35 @@
 // src/Components/HomePage.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './HomePage.css';
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Welcome to the Hospital Management System</h1>
-      <div>
-        <h2>Hospital</h2>
-        <Link to="/hospital-login">
-          <button>Login</button>
-        </Link>
-        <Link to="/hospital-register">
-          <button>Register</button>
-        </Link>
-      </div>
-      <div>
-        <h2>Patient</h2>
-        <Link to="/patient-login">
-          <button>Login</button>
-        </Link>
-        <Link to="/patient-register">
-          <button>Register</button>
-        </Link>
+    <div className="home-container">
+      <h1>Hospital Management System</h1>
+      <div className="buttons-container">
+        <div className="card">
+          <h2>Hospital</h2>
+          <div className="btn-container">
+            <Link to="/hospital-login">
+              <button className="btn">Login</button>
+            </Link>
+            <Link to="/hospital-register">
+              <button className="btn">Register</button>
+            </Link>
+          </div>
+        </div>
+        <div className="card">
+          <h2>Patient</h2>
+          <div className="btn-container">
+            <Link to="/patient-login">
+              <button className="btn">Login</button>
+            </Link>
+            <Link to="/patient-register">
+              <button className="btn">Register</button>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
